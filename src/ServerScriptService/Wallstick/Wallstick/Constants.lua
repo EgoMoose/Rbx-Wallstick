c547c8c10635c6e482e8aa5b0424dafab10209a3
EgoMoose <EgoMoose@users.noreply.github.com>
2020-12-26 13:11:04 -0500

    Remove use of getfenv in constants
    I've heard this disables the use of some Luau optimizations so this was a pretty easy change to make.

--- src/ServerScriptService/Wallstick/Wallstick/Constants.lua
+++ src/ServerScriptService/Wallstick/Wallstick/Constants.lua
@@ -1,30 +1,30 @@
 local PhysicsService = game:GetService("PhysicsService")
 
-WORLD_CENTER = CFrame.new(10000, 0, 0)
-COLLIDER_SIZE2 = Vector3.new(32, 32, 32)
-PHYSICS_ID = PhysicsService:GetCollisionGroupId("WallstickCharacters")
+local CONSTANTS = {}
 
-REPLICATE_RATE = 0.1 -- send an update every x seconds
+CONSTANTS.WORLD_CENTER = CFrame.new(10000, 0, 0)
+CONSTANTS.COLLIDER_SIZE2 = Vector3.new(32, 32, 32)
+CONSTANTS.PHYSICS_ID = PhysicsService:GetCollisionGroupId("WallstickCharacters")
 
-DEBUG = false
-DEBUG_TRANSPARENCY = DEBUG and 0 or 1
+CONSTANTS.REPLICATE_RATE = 0.1 -- send an update every x seconds
 
-DEFAULT_CAMERA_MODE = DEBUG and "Debug" or "Custom"
-CUSTOM_CAMERA_SPIN = true -- if in custom camera match the part spin
-MAINTAIN_WORLD_VELOCITY = true -- maintains world space velocity when using the :Set() method 
+CONSTANTS.DEBUG = false
+CONSTANTS.DEBUG_TRANSPARENCY = CONSTANTS.DEBUG and 0 or 1
 
-IGNORE_CLASS_PART = {
+CONSTANTS.DEFAULT_CAMERA_MODE = CONSTANTS.DEBUG and "Debug" or "Custom"
+CONSTANTS.CUSTOM_CAMERA_SPIN = true -- if in custom camera match the part spin
+CONSTANTS.MAINTAIN_WORLD_VELOCITY = true -- maintains world space velocity when using the :Set() method 
+
+CONSTANTS.IGNORE_CLASS_PART = {
 	["Terrain"] = true,
 	["SpawnLocation"] = true,
 	["Seat"] = true,
 	["VehicleSeat"] = true
 }
 
-IGNORE_STATES = {
+CONSTANTS.IGNORE_STATES = {
 	[Enum.HumanoidStateType.None] = true,
 	[Enum.HumanoidStateType.Dead] = true,
 }
 
-local env = getfenv()
-env.script = nil
-return env
+return CONSTANTS

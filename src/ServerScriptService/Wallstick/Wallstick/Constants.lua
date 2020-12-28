f3199836bedd2ad795cb29bc7e345cc664d22f80
EgoMoose <EgoMoose@users.noreply.github.com>
2020-12-27 22:08:44 -0500

    Add missing comment to constants

--- src/ServerScriptService/Wallstick/Wallstick/Constants.lua
+++ src/ServerScriptService/Wallstick/Wallstick/Constants.lua
@@ -11,7 +11,7 @@ CONSTANTS.REPLICATE_RATE = 0.1 -- send an update every x seconds
 CONSTANTS.DEBUG = false
 CONSTANTS.DEBUG_TRANSPARENCY = CONSTANTS.DEBUG and 0 or 1
 
-CONSTANTS.DEFAULT_CAMERA_MODE = CONSTANTS.DEBUG and "Debug" or "Custom"
+CONSTANTS.DEFAULT_CAMERA_MODE = CONSTANTS.DEBUG and "Debug" or "Custom" -- Custom or Default
 CONSTANTS.CUSTOM_CAMERA_SPIN = true -- if in custom camera match the part spin
 CONSTANTS.MAINTAIN_WORLD_VELOCITY = true -- maintains world space velocity when using the :Set() method 
 

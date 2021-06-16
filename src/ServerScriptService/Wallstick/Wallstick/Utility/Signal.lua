9cdfa9659328149d3231f44bcf9f9ed15ba25991
EgoMoose <EgoMoose@users.noreply.github.com>
2021-06-16 14:56:09 -0400

    Update Signal.lua

--- src/ServerScriptService/Wallstick/Wallstick/Utility/Signal.lua
+++ src/ServerScriptService/Wallstick/Wallstick/Utility/Signal.lua
@@ -1,54 +1,85 @@
--- Class
+-- Taken from Quenty's nevermore engine
+-- https://github.com/Quenty/NevermoreEngine/blob/version2/LICENSE.md
+-- https://github.com/Quenty/NevermoreEngine
 
-local SignalClass = {}
-SignalClass.__index = SignalClass
-SignalClass.ClassName = "Signal"
+--- Lua-side duplication of the API of events on Roblox objects.
+-- Signals are needed for to ensure that for local events objects are passed by
+-- reference rather than by value where possible, as the BindableEvent objects
+-- always pass signal arguments by value, meaning tables will be deep copied.
+-- Roblox's deep copy method parses to a non-lua table compatable format.
+-- @classmod Signal
 
--- Public Constructors
+local ENABLE_TRACEBACK = false
 
-function SignalClass.new()
-	local self = setmetatable({}, SignalClass)
+local Signal = {}
+Signal.__index = Signal
+Signal.ClassName = "Signal"
 
-	self._connections = {}
-	self._bind = Instance.new("BindableEvent")
-	self._params = nil
+--- Constructs a new signal.
+-- @constructor Signal.new()
+-- @treturn Signal
+function Signal.new()
+	local self = setmetatable({}, Signal)
+
+	self._bindableEvent = Instance.new("BindableEvent")
+	self._argData = nil
+	self._argCount = nil -- Prevent edge case of :Fire("A", nil) --> "A" instead of "A", nil
+
+	self._source = ENABLE_TRACEBACK and debug.traceback() or ""
 
 	return self
 end
 
--- Public Methods
-
-function SignalClass:Connect(func)
-	local connection = self._bind.Event:Connect(function()
-		func(unpack(self._params))
-	end)
-	table.insert(self._connections, connection)
-	return connection
-end
-
-function SignalClass:Fire(...)
-	self._params = {...} -- to avoid cyclic table BS
-	self._bind:Fire()
-	self._params = nil
-end
-
-function SignalClass:Wait()
-	return self._bind.Event:Wait()
-end
-
-function SignalClass:Clear()
-	for _, connection in pairs(self._connections) do
-		connection:Dsiconnect()
+--- Fire the event with the given arguments. All handlers will be invoked. Handlers follow
+-- Roblox signal conventions.
+-- @param ... Variable arguments to pass to handler
+-- @treturn nil
+function Signal:Fire(...)
+	if not self._bindableEvent then
+		warn(("Signal is already destroyed. %s"):format(self._source))
+		return
 	end
-	self._connections = {}
+
+	self._argData = {...}
+	self._argCount = select("#", ...)
+	self._bindableEvent:Fire()
+	-- self._argData = nil
+	-- self._argCount = nil
 end
 
-function SignalClass:Destroy()
-	self:Clear()
-	self._bind:Destroy()
-	self._bind = nil
+--- Connect a new handler to the event. Returns a connection object that can be disconnected.
+-- @tparam function handler Function handler called with arguments passed when `:Fire(...)` is called
+-- @treturn Connection Connection object that can be disconnected
+function Signal:Connect(handler)
+	if not (type(handler) == "function") then
+		error(("connect(%s)"):format(typeof(handler)), 2)
+	end
+
+	return self._bindableEvent.Event:Connect(function()
+		handler(unpack(self._argData, 1, self._argCount))
+	end)
 end
 
---
+--- Wait for fire to be called, and return the arguments it was given.
+-- @treturn ... Variable arguments from connection
+function Signal:Wait()
+	self._bindableEvent.Event:Wait()
+	assert(self._argData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
+	return unpack(self._argData, 1, self._argCount)
+end
 
-return SignalClass
+--- Disconnects all connected events to the signal. Voids the signal as unusable.
+-- @treturn nil
+function Signal:Destroy()
+	if self._bindableEvent then
+		self._bindableEvent:Destroy()
+		self._bindableEvent = nil
+	end
+
+	self._argData = nil
+	self._argCount = nil
+
+	setmetatable(self, nil)
+end
+
+return Signal
